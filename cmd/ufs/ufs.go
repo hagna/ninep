5ca54fe59dcb0f46fc74913323071780bb792256
hagna <natester@gmail.com>
2016-06-06 16:08:02 -0600

    this ought to work

--- cmd/ufs/ufs.go
+++ cmd/ufs/ufs.go
@@ -16,20 +16,23 @@ var (
 	ntype      = flag.String("ntype", "tcp4", "Default network type")
 	naddr      = flag.String("addr", ":5640", "Network address")
 	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	ppFile os.File
 )
 
 func profile(s string, i ...interface{}) {
 	switch s {
 	case "Starting readNetPackets":
 		// start profile
-
-		f, err := os.Create(*cpuprofile)
+		log.Println("START")
+		ppFile, err := os.Create(*cpuprofile)
 		if err != nil {
 			log.Fatal(err)
 		}
-		pprof.StartCPUProfile(f)
+		pprof.StartCPUProfile(ppFile)
 	case "Stop readNetPackets":
 		//stop
+		log.Println("STOP!")
+		ppFile.Close()
 		pprof.StopCPUProfile()
 	}
 }
@@ -51,6 +54,7 @@ func main() {
 			s.FromNet, s.ToNet = c, c
 			s.Trace = nil // log.Printf
 			if *cpuprofile != "" {
+
 				s.Trace = profile
 			}
 			return nil
@@ -58,6 +62,7 @@ func main() {
 		if err != nil {
 			log.Printf("Error: %v", err)
 		}
+
 	}
 
 }
